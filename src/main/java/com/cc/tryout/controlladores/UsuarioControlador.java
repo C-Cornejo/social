744dff3a473951cd5d9b12e1535e60047845d1ce
C-Cornejo <castillo.cornejo94@gmail.com>
2018-07-30 21:53:56 -0600

    Remove merge anotations

--- src/main/java/com/cc/tryout/controlladores/UsuarioControlador.java
+++ src/main/java/com/cc/tryout/controlladores/UsuarioControlador.java
@@ -1,20 +1,16 @@
 package com.cc.tryout.controlladores;
 
-<<<<<<< HEAD
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-=======
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
->>>>>>> 832c4d996ab916d28f5adcf3d6dd3daae990123c
 
 import com.cc.tryout.servicios.IUsuarioServicio;
 
@@ -26,13 +22,13 @@ public class UsuarioControlador {
 	IUsuarioServicio servicio;
 	
 	@GetMapping("/todo")
-<<<<<<< HEAD
+
 	public ModelAndView obtenerUsuarios()
 	{
 		ModelAndView model = new ModelAndView("usuario");
 		model.addObject("usuario", servicio.obtenerUsuarios());
 		return model;
-=======
+
 	public ResponseEntity<?> obtenerUsuarios()
 	{
 		try
@@ -43,6 +39,6 @@ public class UsuarioControlador {
 		{
 			return new ResponseEntity<>("ERROR INTERNO DEL SERVER",HttpStatus.INTERNAL_SERVER_ERROR);
 		}
->>>>>>> 832c4d996ab916d28f5adcf3d6dd3daae990123c
+
 	}
 }
